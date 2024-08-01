738d61532c56c766d6c88c3bf62ee06ae0d2bcdf
Md Emon Hasan <iconicemon01@gmail.com>
2024-08-01 17:54:02 +0600

    git commit -m "upload new"

---
+++ test.py
@@ -0,0 +1,19 @@
+# print('hello world')
+# from sklearn.preprocessing import StandardScaler
+# from sklearn.preprocessing import MinMaxScaler
+# from sklearn.datasets import load_iris
+
+# data = load_iris()
+
+# mydict = {
+#     'data'         : data.data,
+#     'target'       : data.target,
+#     'feature_names': data.feature_names,
+#     'target_names' : data.target_names
+# }
+
+# print(mydict)
+
+print('hello','hello')
+for i in range(10):
+    print(i)
